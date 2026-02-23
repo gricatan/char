6aba595131db328ec6c75d6f1d9bc166dfaf71b3
gricatan <132142739+gricatan@users.noreply.github.com>
2026-02-23 18:38:46 +0100

    maj du port en local, mon serveur redirection nginx, pour autre c'est également utile

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=config.SERVER_PORT,
         log_level="info"
     )
